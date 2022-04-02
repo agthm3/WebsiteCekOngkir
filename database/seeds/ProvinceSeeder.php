2c238caa079f3098901877ce5bc5f77ec3967397
Andi Gigatera Halil M <54834402+agthm3@users.noreply.github.com>
2022-04-02 15:29:07 +0800

    main

---
+++ database/seeds/ProvinceSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Province;
+use Illuminate\Database\Seeder;
+use PhpParser\JsonDecoder;
+
+class ProvinceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $file = file_get_contents(base_path('/database/provinsi.json'));
+        $data = json_decode($file, true);
+
+        Province::insert($data);
+    }
+}
