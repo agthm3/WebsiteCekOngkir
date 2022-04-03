6b122e85440c8565fc7f9d2e48d9ad17cb17e219
Andi Gigatera Halil M <54834402+agthm3@users.noreply.github.com>
2022-04-03 22:08:35 +0800

    implementasi select2

--- resources/js/ongkir.js
+++ resources/js/ongkir.js
@@ -3,14 +3,14 @@ $('select[name="origin_province"]').on("change", function () {
 
     if (provinceId) {
         jQuery.ajax({
-            ulr: "/api/province/" + provinceId + "/cities",
+            url: "/api/province/" + provinceId + "/cities",
             type: "GET",
             dataType: "JSON",
             success: function (data) {
                 $('select[name="origin_city"]').empty();
                 $.each(data, function (key, value) {
                     $('select[name="origin_city"]').append(
-                        '<option value="${key}">${value}</option>'
+                        `<option value="${key}"> ${value} </option>`
                     );
                 });
             },
@@ -18,4 +18,25 @@ $('select[name="origin_province"]').on("change", function () {
     } else {
         $('select[name="origin_city"]').empty();
     }
+});
+
+$("#destination_city").select2({
+    ajax: {
+        url: "/api/cities",
+        type: "POST",
+        dataType: "JSON",
+        delay: 150,
+        data: function (params) {
+            return {
+                _token: $('meta[name="csrf-token"]').attr("content"),
+                search: $.trim(params.term),
+            };
+        },
+        processResults: function (response) {
+            return {
+                results: response,
+            };
+        },
+        cache: true,
+    },
 });
